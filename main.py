5f1f10a02861fc900ea437fd22103b2d702e68f1
sohinib12 <sohini.bonthala@gmail.com>
2023-09-18 11:35:43 -0700

    initial skeleton code for class-car

---
+++ main.py
@@ -0,0 +1,30 @@
+class Car:
+    def __init__(self) -> None:
+        pass
+
+    def stop(self):
+        print("car stopped")
+
+    def move_forward(self):
+        print("moving forward")
+
+    def move_backward(self):
+        print("moving backward")
+
+    def move_right(self):
+        print("moving right")
+
+    def move_left(self):
+        print("moving left")
+
+    def look_right(self):
+        print("looking right")
+
+    def look_left(self):
+        print("looking left")
+
+    def look_around(self):
+        print("looking around")
+
+    def turn_off(self):
+        print("Goodbye")
